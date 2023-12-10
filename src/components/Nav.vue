6f8d8624296f646c2009796104f05a1695a48cc9
zzahraJ <r0883814@student.thomasmore.be>
2023-12-10 23:52:32 +0100

    feat: add the nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,9 +2,14 @@
 
 </script>
 <template>
-    <div>
-     NAV
-    </div>
+      <nav class="bg-[#69FF47] flex justify-end h-[50px]">
+        <div class="pr-6 p-2.5">
+            <router-link class="nav-link mx-4 font-bold text-black hover:text-hite text-lg md:text-xl"
+                to="/">Login</router-link>
+            <router-link class="nav-link mx-4 font-bold text-black hover:text-hite text-lg md:text-xl"
+                to="/orders">Orders</router-link>
+        </div>
+    </nav>
   </template>
   
   <style scoped>
