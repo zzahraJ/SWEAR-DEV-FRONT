d6bc56e6d391f0bcf3469765171ae32f50fe1f84
zzahraJ <r0883814@student.thomasmore.be>
2023-12-11 20:20:16 +0100

    feat: add the shoe button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,10 @@
 
 </script>
 <template>
-    <div>
-     HOME
+    <div class="nav-link mx-4 font-bold text-blue hover:text-hite text-lg md:text-l">
+        <router-link exact to="/configurator">
+            <Button text="Make your own shoe"/>
+        </router-link>
     </div>
 </template>
   
