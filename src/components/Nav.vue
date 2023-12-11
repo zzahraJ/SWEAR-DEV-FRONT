be86a19229d192a8be2b08f3719b265dd9b02c39
zzahraJ <r0883814@student.thomasmore.be>
2023-12-11 19:49:43 +0100

    feat: add the configurator tab

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,13 +5,18 @@
       <nav class="bg-[#69FF47] flex justify-end h-[50px]">
         <div class="pr-6 p-2.5">
             <router-link class="nav-link mx-4 font-bold text-black hover:text-hite text-lg md:text-xl"
-                to="/">Login</router-link>
+                to="/">Home
+            </router-link>
             <router-link class="nav-link mx-4 font-bold text-black hover:text-hite text-lg md:text-xl"
-                to="/orders">Orders</router-link>
+                to="/">Configurator
+            </router-link>
+            <router-link class="nav-link mx-4 font-bold text-black hover:text-hite text-lg md:text-xl"
+                to="/admin">Login
+            </router-link>
         </div>
     </nav>
-  </template>
+</template>
   
-  <style scoped>
+<style scoped>
   
-  </style>
+</style>
