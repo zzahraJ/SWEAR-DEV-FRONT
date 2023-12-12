528ca2c38301a6ff79f27f4d095935e43a05995f
zzahraJ <r0883814@student.thomasmore.be>
2023-12-12 10:54:42 +0100

    fix: set the right color

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,15 +2,15 @@
 
 </script>
 <template>
-      <nav class="bg-[#69FF47] flex justify-end h-[50px]">
+      <nav class="bg-black flex justify-end h-[50px]">
         <div class="pr-6 p-2.5">
-            <router-link class="nav-link mx-4 font-bold text-black hover:text-hite text-lg md:text-xl"
+            <router-link class="nav-link mx-4 font-bold text-green-400 hover:text-hite text-lg md:text-xl"
                 to="/">Home
             </router-link>
-            <router-link class="nav-link mx-4 font-bold text-black hover:text-hite text-lg md:text-xl"
+            <router-link class="nav-link mx-4 font-bold text-green-400 hover:text-hite text-lg md:text-xl"
                 to="/configurator">Configurator
             </router-link>
-            <router-link class="nav-link mx-4 font-bold text-black hover:text-hite text-lg md:text-xl"
+            <router-link class="nav-link mx-4 font-bold text-green-400 hover:text-hite text-lg md:text-xl"
                 to="/admin">Login
             </router-link>
         </div>
