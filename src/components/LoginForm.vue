71bebad389cc4ef9a5f4043c5e6177c238572939
zzahraJ <r0883814@student.thomasmore.be>
2023-12-12 10:50:28 +0100

    feat: add the router

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {ref, onMounted} from "vue";
+import router from "../router/router";
+
+</script>
+
+<template>
+  <div class="app-container bg-[#F2F2F2] min-h-screen">
+   
+    
+  </div>
+</template>
+
+<style scoped></style>
