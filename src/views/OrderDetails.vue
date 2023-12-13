7b3949f43d2cd3799d439f4428deae8939143a0f
zzahraJ <r0883814@student.thomasmore.be>
2023-12-13 18:28:28 +0100

    feat: add the style of the orders

--- src/views/OrderDetails.vue
+++ src/views/OrderDetails.vue
@@ -8,12 +8,20 @@ const orderId = computed(() => route.params.id);
 </script>
 
 <template>
-  <div>
-    <h1>Order Details</h1>
-    <p>Order ID: {{ orderId }}</p>
-    <DetailPerOrder :orderId="orderId" />
+  <div class="p-4">
+    <h2 class="text-2xl font-semibold mb-4">Order Details</h2>
+    <div class="bg-green-400 p-4 rounded-md shadow-md">
+      <div class="mb-4">
+        <p class="text-lg font-semibold">
+          Order ID:
+          <span class="ml-2">{{ orderId }}</span>
+        </p>
+      </div>
+      <DetailPerOrder :orderId="orderId" />
+    </div>
   </div>
 </template>
+
 
 <style scoped>
 </style>
